fac99f63170a7374756f50c3c7f0c9dad3102ebd
jp <johannes.p2005@gmail.com>
2021-05-26 06:28:42 +0200

    deleting .zip and deleting empty directory

--- extractor.py
+++ extractor.py
@@ -1,11 +1,16 @@
 import zipfile
 import os
+import shutil
+
 
 def extract(id, filename):
     with zipfile.ZipFile(f'./static/{id}/{filename}', 'r') as zip:
         dir_name= filename.split('.')[0]
-        zip.extractall(f'./static/{id}/{dir_name}')
+        zip.extractall(f'./static/{id}')
 
     lstOfAllFiles= os.listdir(f'./static/{id}/{dir_name}')
-    print(lstOfAllFiles)
-    print('hallo')
+    for element in lstOfAllFiles:
+        shutil.move(f'./static/{id}/{dir_name}/{element}', f'./static/{id}/{element}')
+
+    os.rmdir(f'./static/{id}/{dir_name}')
+    os.remove(f'./static/{id}/{filename}')

da19dbc4b219382208401a6d41aad7aec433b599
jp <johannes.p2005@gmail.com>
2021-05-31 14:13:54 +0200

    process POST data

--- process_yolo.py
+++ process_yolo.py
@@ -1,3 +1,8 @@
+import os
+from pprint import pprint
 
-def process(id, yolo_data):
-    pass
+def process_yolo(id, yolo_data):
+    txtFilename= yolo_data.get('image').replace(yolo_data.get('image').split(id)[1].split('.')[1], "txt").split(id)[1]
+    with open(f'./static/{id}/labels/{txtFilename}', 'a') as file:
+        pprint(yolo_data, indent= 3)
+        file.write(f"{yolo_data['label_index']} {yolo_data['middle_x']} {yolo_data['middle_y']} {yolo_data['width']} {yolo_data['height']}\n")
